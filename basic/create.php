1670e05c0fa016363d97d3cce44c4b365066d0d2
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-10-30 15:22:35 +0700

    feat: added comments

--- basic/create.php
+++ basic/create.php
@@ -1,20 +1,28 @@
 <?php
+// mengimport kode program yang ada didalam database.php
 include 'database.php';
 
+// mengecek apakah ada data bernama 'submit'
 if (isset($_POST['submit'])) {
+    // memasukkan value bersih dari $_POST ke dalam variabel-variabel
     $nama = htmlspecialchars($_POST['nama']);
     $deskripsi = htmlspecialchars($_POST['deskripsi']);
     $harga = htmlspecialchars($_POST['harga']);
 
+    // mengecek apakah $nama dan $harga sudah terisi
     if (isset($nama) && isset($harga)) {
+        // query sql
         $query = "INSERT INTO barangs (nama, deskripsi, harga) VALUES (?, ?, ?)";
+        // parameter atau data yang akan dimasukkan ke dalam tanda tanya (?) di query
         $params = [
             $nama,
             $deskripsi,
             $harga
         ];
-
+        // eksekusi query
         $sql = sqlsrv_query($conn, $query, $params);
+
+        // jika query berhasil maka akan dikembalikan ke halaman index
         if ($sql) {
             header("Location:index.php?msg=create");
         } else {
@@ -40,20 +48,26 @@ if (isset($_POST['submit'])) {
 <body>
     <h3 class="text-center my-3">CREATE</h3>
     <div class="card mx-5 py-2 px-3">
+        <!-- form yang akan menjalankan ulang file create.php dengan method post -->
         <form action="create.php" method="POST">
             <div class="mb-3">
                 <label for="nama" class="form-label">Nama Barang</label>
+                <!-- input nama -->
                 <input type="text" class="form-control" id="nama" name="nama" required>
             </div>
             <div class="mb-3">
                 <label for="deskripsi" class="form-label">Deskripsi Barang</label>
+                <!-- input deskripsi -->
                 <textarea class="form-control" id="deskripsi" name="deskripsi" rows="3"></textarea>
             </div>
             <div class="mb-3">
                 <label for="harga" class="form-label">Harga Barang</label>
+                <!-- input harga -->
                 <input type="number" class="form-control" id="harga" name="harga" required>
             </div>
+            <!-- tombol kirim yang memiliki nama 'submit' -->
             <button class="btn btn-primary" type="submit" name="submit">TAMBAH</button>
+            <!-- tombol reset jika ingin menghapus data yang diinputkan ke form -->
             <button class="btn btn-danger" type="reset">RESET</button>
         </form>
     </div>
--- basic/index.php
+++ basic/index.php
@@ -1,12 +1,19 @@
 <?php
+// mengimport kode program yang ada didalam database.php
 include 'database.php';
 
+// query sql
 $query = "SELECT * FROM barangs";
+// parameter atau data yang akan dimasukkan ke dalam tanda tanya (?) di query
 $sql = sqlsrv_query($conn, $query);
+// deklarasi variabel barang
 $barangs = [];
 
+// cek apabila eksekusi berhasil
 if ($sql) {
+    // looping untuk mengambil baris data dari hasil query dan dikonversi menjadi array asosiatif dan dimasukkan ke dalam variable $row
     while ($row = sqlsrv_fetch_array($sql, SQLSRV_FETCH_ASSOC)) {
+        // setiap baris yang didapat akan di masukkan ke dalam array $barangs
         $barangs[] = $row;
     }
 }
@@ -26,8 +33,11 @@ if ($sql) {
 <body>
     <section class="mx-5 mt-3">
         <?php
+            // mengecek apakah ada value msg di dalam url dengan menggunakan $_get
             if (isset($_GET['msg'])) {
+                // nilai tadi dimasukkan ke variabel $msg untuk di seleksi
                 $msg = htmlspecialchars($_GET['msg']);
+                // seleksi nilai dari variabel, jika sama maka akan di outputkan ke halaman
                 switch ($msg) {
                     case "create":
                         echo "<div class='alert alert-success alert-dismissible fade show'>
@@ -68,14 +78,23 @@ if ($sql) {
                     </tr>
                 </thead>
                 <tbody>
-                    <?php $i = 1;
+                    <?php 
+                    // deklarasi variabel i
+                    $i = 1;
+
+                    // looping variabel $barangs dari kode php diatas dan digunakan sebagai variabel $barang agar dapat di panggil satu per satu dari kumpulan data
                     foreach ($barangs as $barang) { ?>
                         <tr>
+                            <!-- mengoutputkan variabel i untuk penomoran -->
                             <th scope="row"><?= $i++ ?></th>
+                            <!-- mengoutputkan nilai 'nama' dari variabel barang -->
                             <td><?= $barang['nama'] ?></td>
+                            <!-- mengoutputkan nilai 'deskripsi' dari variabel barang -->
                             <td><?= $barang['deskripsi'] ?></td>
+                            <!-- mengoutputkan nilai 'harga' dari variabel barang lalu diformat agar ada komanya -->
                             <td>Rp <?= number_format($barang['harga']) ?></td>
                             <td class="d-flex">
+                                <!-- link yang menuju ke file update dengan membawa value id di dalam urlnya agar dapat diambil dengan $_GET -->
                                 <a href="update.php?id=<?= $barang['id'] ?>" class="btn btn-primary">
                                     <i class="fa-solid fa-pen-to-square"></i></a>
                                 <div class="mx-1"></div>
